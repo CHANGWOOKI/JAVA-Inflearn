c3928c6f25d133aaddb2210e07243c057b23bb50
wook <ckddnr5370@naver.com>
2024-04-23 16:43:52 +0900

    JAVA 입문 교육

---
+++ java-start/src/loop/Nested1.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class Nested1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("외부 for 시작 i = " + i + ", 내부 for j = " + j);
+            }
+        }
+    }
+}
