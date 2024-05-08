7e191cfc2c2c9423297ffbdc92c5628cc6688c12
wook <ckddnr5370@naver.com>
2024-05-08 23:37:16 +0900

    실전 자바 중급 Object

---
+++ java-mid1/src/lang/LangMain.java
@@ -0,0 +1,8 @@
+package lang;
+// 생략가능
+// import java.lang.System
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ java-mid1/src/lang/object/Parent.java
@@ -0,0 +1,10 @@
+package lang.object;
+
+// 부모가 없으면 묵시적으로 Object 클래스를 상속받는다.
+// public class Parent extends Object {
+// 부모 클래스가 없으면 `
+public class Parent {
+    public void parentMethod() {
+
+    }
+}
